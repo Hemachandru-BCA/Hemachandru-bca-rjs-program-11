61c443b023ea0a75f00a24efa0f92d69fd38aed2
Hemachandru <2006hemachandru@gmail.com>
2026-08-14 04:49:09 +0000

    remved comments

--- src/App.js
+++ src/App.js
@@ -2,22 +2,7 @@ import React, { useState } from "react";
 
 function App() {
 
-  // TODO 1:
-  // Create a state variable named isLoggedIn.
   const [isLoggedIn, setLogin] = useState(false);
-  // Initial value should be false.
-
-  // TODO 2:
-  // Use a ternary operator to display:
-  //
-  // If logged in:
-  //     Welcome Student
-  //
-  // Otherwise:
-  //     Login button
-  // The Login button should use onClick
-  // to change the login state.
-
   return (
     <>
       <div>
