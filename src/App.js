bb4b2d3e6423f303387713df67102ab0fd049726
Hemachandru <2006hemachandru@gmail.com>
2026-08-18 05:50:43 +0000

    changed the teneray operator

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ function App() {
   const [isLoggedIn, setLogin] = useState(false);
   return (
     <>
-      <div>
-        <h2>WELCOME TO THE FORM</h2>
-      </div>
-      <div>
-        <form method="GET">
-          <label>Name:</label>
-          <input type="text" /> <br />
-          <label>Dept:</label>
-          <input type="text" /> <br />
-          <label>RollNo:</label>
-          <input type="text" /><br />
+      {isLoggedIn ? (
+        <h2>Welcome Student</h2>
+        ) : (
           <div>
-          {
-            isLoggedIn ? (<h2>Welcome Student</h2>) : 
-            (<button type="button" onClick={() => setLogin(true)}>Login</button>)
-          }
+            <div>
+              <h2>WELCOME TO THE FORM</h2>
+            </div>
+
+            <div>
+              <form method="GET">
+                <label>Name:</label>
+                <input type="text" /> <br />
+                <label>Dept:</label>
+                <input type="text" /> <br />
+                <label>RollNo:</label>
+                <input type="text" /><br />
+
+                <div>
+                  <button type="button" onClick={() => setLogin(true)}>Login</button>
+                </div>
+                
+              </form>
+            </div>
           </div>
-        </form>
-      </div>
+        )}
     </>
   );
 
