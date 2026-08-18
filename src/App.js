3553cf565f64710af4184d15e4c099e3593f91b0
Hemachandru <2006hemachandru@gmail.com>
2026-08-18 06:27:35 +0000

    updated build

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function App() {
-
+  const inputRef = useRef();
   const [isLoggedIn, setLogin] = useState(false);
+  const [studentName, setStudentName] = useState("");
+
+  function onFocus() {
+    const st_nm = inputRef.current.value;
+    if (st_nm == "") {
+      setLogin(false);
+      inputRef.current.focus();
+    }
+    else {
+      setLogin(true);
+      setStudentName(st_nm);
+    }
+
+
+  }
   return (
     <>
       {isLoggedIn ? (
-        <h2>Welcome Student</h2>
-        ) : (
+        <div>
+          <h2>Welcome {studentName}</h2> <br />
+        </div>
+
+      ) : (
+        <div>
           <div>
-            <div>
-              <h2>WELCOME TO THE FORM</h2>
-            </div>
-
-            <div>
-              <form method="GET">
-                <label>Name:</label>
-                <input type="text" /> <br />
-                <label>Dept:</label>
-                <input type="text" /> <br />
-                <label>RollNo:</label>
-                <input type="text" /><br />
-
-                <div>
-                  <button type="button" onClick={() => setLogin(true)}>Login</button>
-                </div>
-                
-              </form>
-            </div>
+            <h2>WELCOME TO THE FORM</h2>
           </div>
-        )}
+
+          <div>
+            <form method="GET">
+              <label>Name:</label>
+              <input type="text" ref={inputRef} /> <br />
+              <label>Dept:</label>
+              <input type="text" /> <br />
+              <label>RollNo:</label>
+              <input type="text" /><br />
+
+              <div>
+                <button type="button" onClick={onFocus}>Login</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      )}
     </>
   );
 
